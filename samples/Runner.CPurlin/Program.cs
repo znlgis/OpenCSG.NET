99836665fb586b4cc82319730dce6f2e3ddea442
znlgis <znlgis@foxmail.com>
2026-07-31 18:06:58 +0800

    samples: add Runner.Examples and Runner.CPurlin

---
+++ samples/Runner.CPurlin/Program.cs
@@ -0,0 +1,144 @@
+﻿using Csg;
+using static Csg.Solids;
+using System.Diagnostics;
+
+const bool debug = false;
+
+var web = 152.0;
+var flange = 64.0;
+var lip = 18.5;
+var thickness = 2.4;
+
+var holeSize1 = 18.0;
+var holeSize2 = 22.0;
+
+var webCenter = 60.0;
+var punchingsWeb = new List<double>() { 35 };
+var punchingsFlange = new List<double>() { 70 };
+var punchingsCenter = new List<double>() { 105 };
+
+var height = 1000.0;
+
+var r = 5.0;
+
+var h1 = debug ? height * 1.2 : height;
+var h2 = debug ? height * 2.0 : height;
+var h3 = debug ? height * 2.2 : height;
+var h4 = debug ? height * 3.0 : height;
+
+var sweb = Cube(
+    center: V3D(-flange / 2 + thickness / 2, 0, 0),
+    size: V3D(thickness, web - r * 2, h1));
+
+foreach (var p in punchingsWeb)
+{
+    var holeTop = Punching(p, true)
+        .Translate(-flange / 2, +webCenter / 2, 0);
+    var holeBottom = Punching(p, true)
+        .Translate(-flange / 2, -webCenter / 2, 0);
+    sweb = sweb.Subtract(holeTop, holeBottom);
+}
+
+foreach (var p in punchingsCenter)
+{
+    var holeCenter = Punching(p, true)
+        .Translate(-flange / 2, 0, 0);
+    sweb = sweb.Subtract(holeCenter);
+}
+
+var sflangetop = Cube(
+    center: V3D(0, web / 2 - thickness / 2, 0),
+    size: V3D(flange - r * 2, thickness, h1));
+
+var sflangebottom = Cube(
+    center: V3D(0, -web / 2 + thickness / 2, 0),
+    size: V3D(flange - r * 2, thickness, h1));
+
+foreach (var p in punchingsFlange)
+{
+    var holeTop = Punching(p)
+        .Translate(0, +web / 2, 0);
+    sflangetop = sflangetop.Subtract(holeTop);
+
+    var holeBottom = Punching(p)
+        .Translate(0, -web / 2, 0);
+    sflangebottom = sflangebottom.Subtract(holeBottom);
+}
+
+var sliptop = Cube(
+    center: V3D(flange / 2 - thickness / 2, web / 2 - r / 2 - lip / 2, 0),
+    size: V3D(thickness, lip - r, h1));
+
+var slipbottom = Cube(
+    center: V3D(flange / 2 - thickness / 2, -web / 2 + r / 2 + lip / 2, 0),
+    size: V3D(thickness, lip - r, h1));
+
+var notch = Cube(web, web, web, true)
+    .Translate(0, 0 - web + lip * 1.5, height / 2);
+
+var s = Union(sweb, sflangetop, sflangebottom, sliptop, slipbottom, Corner(-1, +1), Corner(+1, +1), Corner(-1, -1), Corner(+1, -1))
+    .RotateX(90)
+    .Translate(0, height / 2, 0)
+    .RotateX(90);
+
+s = s.Subtract(notch);
+
+var outputName = "CPurlin";
+var outputTextPath = outputName + "-text.stl";
+var outputBinPath = outputName + "-bin.stl";
+
+using (var fs = File.Create(outputTextPath))
+using (var sw = new StreamWriter(fs))
+{
+    s.WriteStl(outputName, sw);
+}
+
+using (var fs = File.Create(outputBinPath))
+using (var bw = new BinaryWriter(fs))
+{
+    s.WriteStl(outputName, bw);
+}
+
+Process.Start(new ProcessStartInfo()
+{
+    FileName = outputTextPath,
+    UseShellExecute = true
+});
+
+Solid Corner(int x, int y)
+{
+    return Cylinder(r, h2, true)
+        .Subtract(Cylinder(r - thickness, h3, true))
+        .RotateX(90)
+        .Intersect(Cube(
+            center: V3D(x * r / 2, y * r / 2, 0),
+            size: V3D(r, r, h4)))
+        .Translate(x * flange / 2 + -x * r, y * web / 2 + -y * r, 0);
+}
+
+Solid Punching(double z, bool onWeb = false)
+{
+    var punching = Cylinder(holeSize1 / 2, thickness * 4, true);
+
+    if (holeSize2 > holeSize1)
+    {
+        // Pill-shaped hole instead
+
+        var diff = holeSize2 - holeSize1;
+
+        punching = Union(
+            punching.Translate(z: -diff / 2),
+            punching.Translate(z: +diff / 2),
+            Cube(V3D(holeSize1, thickness * 4, diff), center: true)
+        );
+    }
+
+    if (onWeb)
+    {
+        punching = punching.RotateZ(90);
+    }
+
+    return punching.Translate(0, 0, height / 2 - z);
+}
+
+static Vector3D V3D(double x, double y, double z) => new(x, y, z);
---
+++ samples/Runner.Examples/Program.cs
@@ -0,0 +1,162 @@
+﻿// All types in this library are in the Csg namespace
+using Csg;
+
+// Use this for simpler calls to solids and operations
+using static Csg.Solids;
+
+Solid allSolids = new();
+double allX = 0;
+void add(params Solid[] solids)
+{
+    allX += 5;
+
+    allSolids = Union(
+        allSolids,
+        Union(solids).Translate(x: allX)
+    );
+}
+
+//
+// Basic Solids
+//
+
+// Create a basic cube
+var cube = Cube(size: 1, center: true);
+
+// Create a basic sphere
+var sphere = Sphere(r: 0.5, center: true);
+
+// Create a basic cylinder
+var cylinder = Cylinder(r: 0.5, h: 1, center: true);
+
+add(cube);
+add(sphere);
+add(cylinder);
+
+//
+// Moving (Translate)
+//
+
+// Create a small cube in the center
+var smallCube = Cube(size: 0.4, center: true);
+
+// Have the cube to the right (x-axis)
+var moveCube = cube.Translate(x: +2);
+
+// Have a sphere forward (y-axis)
+var moveSphere = sphere.Translate(y: +2);
+
+// Have a cylinder up (z-axis)
+var moveCylinder = cylinder.Translate(z: +2);
+
+add(smallCube, moveCube, moveSphere, moveCylinder);
+
+//
+// Operations
+//
+
+var offsetSphere = sphere.Translate(x: 0.5, y: 0.5, z: 0.5);
+
+// Union combines the solids together, even if there's no shared area
+var union = Union(cube, offsetSphere);
+
+// Difference removes the second solid, taking the shared area from the first solid as well
+var difference = Difference(cube, offsetSphere);
+
+// Intersection keeps on the shared area of both solids, if there's no shared area then it returns empty space
+var intersection = Intersection(cube, offsetSphere);
+
+// Note that you can also call the operations via member functions with no difference to the object returned
+//var union = cube.Union(offsetSphere);
+//var difference = cube.Subtract(offsetSphere);
+//var intersection = cube.Intersect(offsetSphere);
+
+add(union);
+add(difference);
+add(intersection);
+
+//
+// Example: Barbell via union
+//
+
+var bar = Cylinder(r: 0.025, h: 2.1, center: true);
+
+var weight = Sphere(r: 0.2, center: true);
+
+// Note that we're using the same weight object more than once
+// Every function that returns a Solid will not modify the original object
+// So you can create your geometry and then place it in different locations
+
+var barbell = Union(bar,
+    weight.Translate(y: -0.9),
+    weight.Translate(y: +0.9)
+);
+
+add(barbell);
+
+//
+// Example: Pipe via Difference
+//
+
+var outer = Cylinder(r: 1, h: 4, center: true);
+var inner = Cylinder(r: 0.95, h: 4, center: true);
+
+var pipe = Difference(outer, inner);
+
+add(pipe);
+
+//
+// Writing a solid to an STL file
+//
+
+using (var fs = File.Create($"cube.stl"))
+using (var wr = new StreamWriter(fs))
+//using (var wr = new BinaryWriter(fs)) // Note you can also use BinaryWriter to write as an STL binary file
+{
+    cube.WriteStl("cube", wr);
+}
+
+//
+// Complete example: Neat geometry on the CSG Wikipedia page and saving it to a file
+//
+
+var rod = Cylinder(r: 0.5, h: 2, center: true);
+
+var wiki = Difference(
+    Intersection(
+        Cube(size: 1.4, center: true),
+        Sphere(r: 1, center: true)
+    ),
+    Union(
+        rod,
+        rod.RotateX(90),
+        rod.RotateZ(90)
+    )
+);
+
+using (var fs = File.Create($"wiki.stl"))
+using (var wr = new StreamWriter(fs))
+//using (var wr = new BinaryWriter(fs)) // Note you can also use BinaryWriter to write as an STL binary file
+{
+    wiki.WriteStl("wiki", wr);
+}
+
+add(wiki);
+
+//
+// Finish by creating the STL file and opening it
+//
+
+// Write the file to STL
+using (var fs = File.Create($"CsgExamples.stl"))
+using (var sw = new StreamWriter(fs))
+{
+    allSolids.WriteStl("CsgExamples", sw);
+}
+
+// Open the file in the default STL viewer (e.g. "3D Viewer" on Windows)
+System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo()
+{
+    FileName = $"CsgExamples.stl",
+    UseShellExecute = true
+});
